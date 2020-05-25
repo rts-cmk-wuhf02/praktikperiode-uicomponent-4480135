f58f3a6e5f8073608dadc555742a9a27de1af2a3
4480135 <4480135@RTS-365.dk>
2020-05-25 11:11:43 +0200

    Remove Netlify test and prepare for component development.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ContentfulClient, ContentfulProvider, Query } from "react-contentful";
+import { ContentfulClient, ContentfulProvider } from "react-contentful";
 import "./App.css";
 
 const contentfulClient = new ContentfulClient({
@@ -10,31 +10,7 @@ const contentfulClient = new ContentfulClient({
 function App() {
     return (
         <ContentfulProvider client={contentfulClient}>
-            <div className="App">
-                <Query contentType="product" query={{}}>
-                    {({ data, error, fetched, loading }) => {
-                        if (loading || !fetched) {
-                            return null;
-                        }
-
-                        if (error) {
-                            console.error(error);
-                            console.log(process.env.REACT_APP_ACCESS_TOKEN);
-                            return null;
-                        }
-
-                        if (!data) {
-                            return <p>Page does not exist.</p>;
-                        }
-
-                        // See the Contentful query response
-                        console.log(data);
-
-                        // Process and pass in the loaded `data` necessary for your page or child components.
-                        return JSON.stringify(data);
-                    }}
-                </Query>
-            </div>
+            <div className="App"></div>
         </ContentfulProvider>
     );
 }
